af198e782eacb8876ec2347174d0a91646b859a2
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-10-06 12:27:36 +0330

    create structure

---
+++ tests/main.py
@@ -0,0 +1,11 @@
+import unittest
+from unittest.main import main
+
+
+class TestOfTestSystem(unittest.TestCase):
+    def testIterate(self):
+        assert True == True
+
+if __name__ == '__main__':
+    unittest.main()
+    
