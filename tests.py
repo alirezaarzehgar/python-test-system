ef72757ad4f246a26d877543dfc88e22001a9322
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-10-06 13:30:19 +0330

    cleanup tests

--- tests.py
+++ tests.py
@@ -7,16 +7,38 @@ from TestSystem.Runable import Runable
 
 class A(Runable):
     def run():
-        print("hello")
+        print("hello!")
+        return True
 
+class B():
+    pass
+
+class C(Runable):
+    pass
+
+class D(Runable):
+    def run():
+        print("without return")
 
 class TestOfTestSystem(unittest.TestCase):
-    def testIterate(self):
-        TestSystem().iterate([
+    def testIterateFail(self):
+        results = TestSystem().iterate([
+            None,
+            B,
+            C,
+            D,
+        ])
+
+        for result in results:
+            assert not result
+
+    def testIteratePass(args):
+        results = TestSystem().iterate([
             A
         ])
 
-        assert True == True
+        for result in results:
+            assert result == True
 
 if __name__ == '__main__':
     unittest.main()
