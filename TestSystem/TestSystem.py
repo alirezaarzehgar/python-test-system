1985ed739eb914eb94a932f48ca6967cb1864a67
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-10-06 13:30:33 +0330

    error handling for test system

--- TestSystem/TestSystem.py
+++ TestSystem/TestSystem.py
@@ -9,9 +9,26 @@ class TestSystem():
         color = Color()
         self.report = Report(color, True)
 
-    def iterate(self, classes: List[Runable]):
+    def iterate(self, classes: List[Runable]) -> List[bool]:
+        result = []
+
         for className in classes:
-            if hasattr(className, "run"):
-                className.run()
+            if className != None and hasattr(className, "run"):
+                ret = None
+
+                try:
+                    ret = className.run()
+                except:
+                    ret = False
+
+                if ret == None:
+                    ret = False
+
+                result.append(ret)
+
             else:
                 self.report.printError("has no attribute 'run'")
+
+                result.append(False)
+
+        return result
